c601f54ce3e1c8d5d870abf508d6e7478c92ec1b
nicholaspace <nicholaspace97@yahoo.ca>
2015-06-07 22:31:25 -0400

    the real way to add v4a
    Change-Id: Ibe6c613c437b3c2e74a002c06d68e86c15550a9c

---
+++ configs/addons.mk
@@ -0,0 +1,10 @@
+#Viper4Android
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/etc/v4a/91-v4a.sh:system/addon.d/91-v4a.sh \
+    vendor/pa/prebuilt/apk/ViPER4Android_FX_A4.x.apk:system/priv-app/ViPER4Android_FX_A4/ViPER4Android_FX_A4.x.apk \
+    vendor/pa/prebuilt/etc/v4a/libs/armeabi/libV4AJniUtils.so:system/priv-app/ViPER4Android_FX_A4/lib/arm/libV4AJniUtils.so \
+    vendor/pa/prebuilt/etc/audio_effects.conf:system/etc/audio_effects.conf \
+    vendor/pa/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    vendor/pa/prebuilt/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    vendor/pa/prebuilt/etc/soundfx/libv4a_fx_ics.so:system/lib/soundfx/libv4a_fx_ics.so \
+
